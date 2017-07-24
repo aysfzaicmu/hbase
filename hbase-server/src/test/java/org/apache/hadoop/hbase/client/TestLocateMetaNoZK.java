093136c38f8646a51356f52f46ec06433c28504a
Ali Yousafzai <ayousafzai@salesforce.com>
2017-07-24 16:17:40 -0700

    renamed test file to something more appropriate
    Signed-off-by: Ali Yousafzai <ayousafzai@salesforce.com>

--- hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestLocateMetaNoZK.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/client/TestLocateMetaNoZK.java
@@ -1,6 +1,5 @@
 package org.apache.hadoop.hbase.client;
 
-
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -9,14 +8,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HRegionLocation;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.MiniHBaseCluster;
-import org.apache.hadoop.hbase.RegionLocations;
-import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.master.HMaster;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.junit.After;
@@ -29,8 +24,8 @@ import org.junit.experimental.categories.Category;
 import org.junit.rules.TestName;
 
 @Category({ MediumTests.class })
-public class TestFindActiveMaster {
-  private static final Log LOG = LogFactory.getLog(TestFindActiveMaster.class);
+public class TestLocateMetaNoZK {
+  private static final Log LOG = LogFactory.getLog(TestLocateMetaNoZK.class);
   private final static HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
   private Admin admin;
   public static final int NUM_MASTERS = 3;
@@ -46,9 +41,6 @@ public class TestFindActiveMaster {
     TEST_UTIL.getConfiguration().setInt("hbase.client.retries.number", 6);
     TEST_UTIL.getConfiguration().setInt("hbase.regionserver.metahandler.count", 30);
     TEST_UTIL.getConfiguration().setBoolean("hbase.master.enabletable.roundrobin", true);
-    // locs of masters(backups as well) comma separated
-    // TEST_UTIL.getConfiguration().set("hbase.master.all",
-    // "localhost,-1,-1;localhost,8282,1122212122;localhost,5543,131141413");
     TEST_UTIL.startMiniCluster(NUM_MASTERS, 3, false);
   }
 
@@ -61,16 +53,10 @@ public class TestFindActiveMaster {
   public void setUp() throws Exception {
     Configuration conf = TEST_UTIL.getConfiguration();
     String master_locs = constructMasterLocsStr();
-    conf.set("hbase.master.all", master_locs);
-
     Configuration newConf = new Configuration(conf);
     newConf.set("hbase.master.all", master_locs);
-
     conn = (ConnectionImplementation) ConnectionFactory.createConnection(newConf);
-
-    // conn = (ConnectionImplementation) ConnectionFactory.createConnection(conf);// PASS IN CLONED
-    this.admin = conn.getAdmin(); // VERSIONNNNNNN
-
+    this.admin = conn.getAdmin();
   }
 
   @After
@@ -85,17 +71,14 @@ public class TestFindActiveMaster {
    * rpc from client to master serialized and deserialized locations correctly
    */
   @Test
-  public void testLocateMeta() throws IOException {
+  public void testMetaSentCorrectly() throws IOException {
     MiniHBaseCluster cluster = TEST_UTIL.getHBaseCluster();
     HMaster m = cluster.getMaster();
     HRegionLocation[] metaLocsFromMaster = m.locateMeta().getRegionLocations();
-
-    // connection calls admin for locate meta
-    HRegionLocation[] metaLocsFromConnection = admin.locateMeta().getRegionLocations();
+    HRegionLocation[] metaLocsFromConnection = conn.getAdmin().locateMeta().getRegionLocations();
 
     assertEquals(metaLocsFromMaster.length, metaLocsFromConnection.length);
     for (int i = 0; i < metaLocsFromMaster.length; i++) {
-
       HRegionLocation locFromMaster = metaLocsFromMaster[i];
       HRegionLocation locFromConnection = metaLocsFromConnection[i];
       assertEquals(locFromMaster.getServerName(), locFromConnection.getServerName());
@@ -109,32 +92,28 @@ public class TestFindActiveMaster {
     }
   }
 
-
+  /**
+   * This test ensures that the meta location found from client->connection->ZooKeeper is same as
+   * client->connection->Master->ZooKeeper
+   */
   @Test
-  public void testFoo() throws IOException {
-    System.out.println("in test find active master");
-
-    final String name = this.name.getMethodName();
-    HTableDescriptor htd1 = new HTableDescriptor(TableName.valueOf(name));
-    htd1.addFamily(new HColumnDescriptor(HConstants.CATALOG_FAMILY));
-    this.admin.createTable(htd1);
-    // conn.getTable(htd1.getTableName()).close();
-
-    Table table1 = conn.getTable(TableName.valueOf(name));
-    // List<HRegionLocation> table1Loc = conn.locateRegions(table1.getName());
-    RegionLocations metaLocs = conn.locateMeta(table1.getName(), false, -1);
-    // System.out.println("table1Loc " + table1Loc.toString());
-    System.out.println("meta locs " + metaLocs.toString());
-
-    HMaster firstMaster = TEST_UTIL.getMiniHBaseCluster().getMaster(0);
-
-    System.out.println("aborting master " + firstMaster.getServerName());
-    // firstMaster.stopMaster();
-    firstMaster.abort(null);
-
-    RegionLocations metaLocsDuplicate = conn.locateMeta(table1.getName(), false, -1);
-    System.out.println("meta locs duplicate" + metaLocsDuplicate.toString());
-
+  public void testMetaFromMaster() throws IOException {
+    MiniHBaseCluster cluster = TEST_UTIL.getHBaseCluster();
+    HMaster m = cluster.getMaster();
+    HRegionLocation[] metaLocsViaMaster = conn.getAdmin().locateMeta().getRegionLocations();
+    HRegionLocation[] metaLocsFromZK = conn.registry.getMetaRegionLocation().getRegionLocations();
+    assertEquals(metaLocsViaMaster.length, metaLocsFromZK.length);
+    for (int i = 0; i < metaLocsViaMaster.length; i++) {
+      HRegionLocation locFromMaster = metaLocsViaMaster[i];
+      HRegionLocation locFromZK = metaLocsFromZK[i];
+      assertEquals(locFromMaster.getServerName(), locFromZK.getServerName());
+      assertEquals(locFromMaster.getSeqNum(), locFromZK.getSeqNum());
+      HRegionInfo rlFromMaster = locFromMaster.getRegionInfo();
+      HRegionInfo rlFromZK = locFromZK.getRegionInfo();
+      assertEquals(rlFromMaster.getTable(), rlFromMaster.getTable());
+      assertEquals(rlFromMaster.getReplicaId(), rlFromZK.getReplicaId());
+      assertEquals(rlFromMaster.getRegionId(), rlFromZK.getRegionId());
+    }
   }
 
   public String constructMasterLocsStr() {
